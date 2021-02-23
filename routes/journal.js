bdbb4a548b53f4a0d9b9ec2f1e09698d3fd04174
GBelonozhko <44050866+GBelonozhko@users.noreply.github.com>
2021-02-22 16:40:06 -0800

    g

--- routes/journal.js
+++ routes/journal.js
@@ -2,20 +2,34 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const Journal = require("../models/journal");
+const journal = require("../models/journal");
 
 router.post("/addjournal/", (req, res) => {
   const journalEntry = new Journal(req.body.journalEntry);
   const userId = req.body.journalEntry.creator;
 
-  journalEntry.save().then(() => {
-    return User.findById(userId);
-  }).then((user)=>{
+  journalEntry
+    .save()
+    .then(() => {
+      return User.findById(userId);
+    })
+    .then((user) => {
       user.journal.push(journalEntry);
       return user.save();
-  });
-  res.json(journalEntry)
+    });
+  res.json(journalEntry);
 });
 
-router.get(`/journal`)
+router.get("/alljournals/:userId", (req, res) => {
+  Journal.find({
+    creator: req.params.userId,
+  }).then((journal) => {
+    res.status(200).json({
+      journals: journal,
+    });
+  });
+});
+
+
 
 module.exports = router;
